f1c2a1a281ce92176adcf1bda29ca8643d24937b
mauricio.sierrac <2376@holbertonschool.com>
2020-09-17 23:49:24 +0000

    task4

---
+++ 0x01-variables_if_else_while/4-print_alphabt.c
@@ -0,0 +1,25 @@
+#include <stdio.h>
+
+/**
+ * main - remove two letters from the alphabet
+ *
+ * Return: 0
+ **/
+
+int main(void)
+{
+  int x = 'a';
+  int c = 'a';
+  while (x <= 'z')
+    {
+      if (c != 'e' && c != 'q')
+	{
+      putchar(c);
+	}
+      x++;
+      c++;
+	
+}
+  putchar('\n');
+  return (0);
+}
