7bd9318322abd313ee151f322edfd5e1a5f5a56e
mauricio.sierrac <2376@holbertonschool.com>
2020-09-18 04:17:20 +0000

    task8

---
+++ 0x01-variables_if_else_while/8-print_base16.c
@@ -0,0 +1,30 @@
+#include <stdio.h>
+
+/**
+* main print number of base16
+*
+* Return: 0
+**/
+
+int main(void)
+
+{
+
+int x = 48;
+int y = 97;
+
+while (x < 58)
+{
+putchar(x);
+x++;
+}
+while (y < 103)
+{
+putchar(y);
+y++;
+}
+
+putchar('\n');
+
+return (0);
+}
