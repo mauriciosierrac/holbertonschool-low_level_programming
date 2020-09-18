fe6469544fc4ddcc55e09a62948412424613e68d
mauricio.sierrac <2376@holbertonschool.com>
2020-09-18 02:17:52 +0000

    tas10

---
+++ 0x01-variables_if_else_while/10-print_comb2.c
@@ -0,0 +1,40 @@
+#include <stdio.h>
+
+/**
+* main - display numbers 00 to 99
+*
+* Return: 0
+**/
+
+int main(void)
+{
+int x = 48;
+int y = 48;
+
+while (x < 58)
+{
+
+while (y < 58)
+{
+
+putchar(x);
+putchar(y);
+if (x != 57 || y != 57)
+{
+putchar(',');
+}
+putchar(32);
+y++;
+
+}
+
+y = 48;
+x++;
+
+}
+
+
+putchar('\n');
+return (0);
+}
+
