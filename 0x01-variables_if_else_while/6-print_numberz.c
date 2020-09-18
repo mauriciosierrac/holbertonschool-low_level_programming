a108c79380bbf29549c715664a4d3511624bc6cf
mauricio.sierrac <2376@holbertonschool.com>
2020-09-18 00:18:06 +0000

    task6

---
+++ 0x01-variables_if_else_while/6-print_numberz.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+
+/**
+* main - display numbers 0 to 9 with putchar
+*
+* Return: 0
+**/
+
+int main(void)
+{
+int x = 48;
+
+while (x < 58)
+{
+putchar(x);
+x++;
+}
+putchar('\n');
+return (0);
+}
+
+
