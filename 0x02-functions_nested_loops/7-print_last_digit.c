9d7e78602ef32bb53db95c830f4d5e01afe8951e
mauricio.sierrac <2376@holbertonschool.com>
2020-09-23 14:39:35 +0000

    uploadtask7

--- 0x02-functions_nested_loops/7-print_last_digit.c
+++ 0x02-functions_nested_loops/7-print_last_digit.c
@@ -18,18 +18,18 @@ if (n < 0)
 {
 n = n * -1;
 last = n % 10;
-_putchar(last + 48);
+_putchar(last + '0');
 }
 else
 if (n > 0)
 {
 last = n % 10;
-_putchar(last + 48);
+_putchar(last + '0');
 }
 else
 {
 last = n;
-_putchar(last + 48);
+_putchar(last + '0');
 }
 
 return (last);
