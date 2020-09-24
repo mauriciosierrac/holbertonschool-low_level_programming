6bd9f1cdda8cafadca0d20eb050a9f2151398c65
mauricio.sierrac <2376@holbertonschool.com>
2020-09-24 18:10:53 +0000

    task5

---
+++ 0x04-more_functions_nested_loops/4-print_most_numbers.c
@@ -0,0 +1,25 @@
+#include "holberton.h"
+
+/**
+ * print_most_numbers - print digits 0 to 9
+ *
+ * Return void
+ **/
+
+void print_most_numbers(void)
+{
+
+int a = 0;
+
+while (a <= 9)
+{
+if (a != 2 && a != 4)
+{
+_putchar(a + '0');
+}
+a++;
+}
+
+_putchar('\n');
+}
+
---
+++ 0x04-more_functions_nested_loops/5-more_numbers.c
@@ -0,0 +1,29 @@
+#include "holberton.h"
+
+/**
+ * more_numbers - print numbers 0 to 19, 10 times
+ *
+ * Return: void
+ *
+ **/
+
+
+void more_numbers(void)
+{
+int a = 0, b;
+
+for (b = 0; b <= 10; b++)
+{
+while (a <= 14)
+{
+if (a > 9)
+{
+_putchar((a / 10) + '0');
+}
+_putchar((a % 10) + '0');
+a++;
+}
+a = 0;
+_putchar('\n');
+}
+}
