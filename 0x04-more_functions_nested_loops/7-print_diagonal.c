a52442541cdea525c8e5a9f9123358845578c40b
mauricio.sierrac <2376@holbertonschool.com>
2020-09-24 20:33:51 +0000

    task7

---
+++ 0x04-more_functions_nested_loops/7-print_diagonal.c
@@ -0,0 +1,29 @@
+#include "holberton.h"
+
+/**
+ * print_diagonal - print
+ * @n: variable
+ *
+ **/
+
+
+void print_diagonal(int n)
+{
+
+int a;
+
+while (a <= n)
+{
+if (a < n)
+{
+_putchar(' ');
+}
+if (a == n)
+{
+_putchar('\\');
+}
+a++;
+}
+_putchar('\n');
+
+}
