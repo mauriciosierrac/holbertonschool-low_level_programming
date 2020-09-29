9dd7085ad1cb4855efcae4d4becc68bfc311d1bd
mauricio.sierrac <2376@holbertonschool.com>
2020-09-29 15:41:38 +0000

    task6

--- 0x05-pointers_arrays_strings/6-puts2.c
+++ 0x05-pointers_arrays_strings/6-puts2.c
@@ -10,17 +10,14 @@
 void puts2(char *str)
 {
 
-  int len;
+int len;
 
-  for (len = 0; *(str + len) != '\0'; len++)
-    {
-      if (len % 2 == 0)
-	{
-      _putchar(*(str + len));
-	}
-    }
-
-
-  _putchar('\n');
-
+for (len = 0; *(str + len) != '\0'; len++)
+{
+if (len % 2 == 0)
+{
+_putchar(*(str + len));
+}
+}
+_putchar('\n');
 }
