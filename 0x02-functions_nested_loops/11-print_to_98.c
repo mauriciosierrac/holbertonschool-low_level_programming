ca4be8e087366e7e188b5c3b9b4c2af8436f82e7
mauricio.sierrac <2376@holbertonschool.com>
2020-09-30 02:41:37 +0000

    task11update

---
+++ 0x02-functions_nested_loops/11-print_to_98.c
@@ -0,0 +1,18 @@
+#include "holberton.h"
+
+/**
+ * print_to_98 - print numbers from n to 98
+ *
+ * @n: variable
+ **/
+
+void print_to_98(int n)
+{
+for (; n > 98; n--)
+printf("%d, ", n);
+for (; n < 98; n++)
+printf("%d, ", n);
+printf("98\n");
+}
+
+
--- 0x02-functions_nested_loops/9-times_table.c
+++ 0x02-functions_nested_loops/9-times_table.c
@@ -6,40 +6,44 @@
 * Return: void
 **/
 
+
+
 void times_table(void)
 {
 
-int a = 0, b = 0, c;
+  int a = 0, b = 0, c;
 
-while (a < 10)
-{
-while (b < 10)
-{
-c = a * b;
-if (c > 9)
-{
-_putchar(',');
-_putchar(' ');
-_putchar((c / 10) + '0');
-_putchar((c % 10) + '0');
+  while (a < 10)
+    {
+      while (b < 10)
+	{
+	  c = a * b;
+	  if (c > 0 && c <= 9)
+	    {
+	      _putchar(',');
+	      _putchar(' ');
+	      _putchar(' ');
+	      _putchar(c + '0');
+	    }
+	  else
+	    if (c > 9)
+	      {
+		_putchar(',');
+		_putchar(' ');
+		_putchar((c / 10) + '0');
+		_putchar((c % 10) + '0');
 
-}
-else
-if (c > 0 && c <= 9)
-{
-_putchar(',');
-_putchar(' ');
-_putchar(' ');
-_putchar(c + '0');
-}
-else
-{
-_putchar(c + '0');
-}
-b++;
-}
-a++;
-b = 0;
-_putchar('\n');
-}
+	      }
+	    else
+	      {
+		_putchar(c + '0');
+	      }
+
+	  b++;
+	}
+
+      a++;
+      b = 0;
+      _putchar('\n');
+    }
 }
