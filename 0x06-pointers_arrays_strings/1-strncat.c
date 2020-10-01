b5d06cbef1a80f0a948f0f298d68fffedd3ad2f6
mauricio.sierrac <2376@holbertonschool.com>
2020-10-01 19:49:21 +0000

    task1

---
+++ 0x06-pointers_arrays_strings/1-strncat.c
@@ -0,0 +1,27 @@
+B#include "holberton.h"
+
+/**
+ * _strncat - print concatenated
+ *
+ * @dest: variable1
+ * @src: variable2
+ * @n: variable 3
+ * Return: contatenated
+ **/
+
+char *_strncat(char *dest, char *src, int n)
+{
+
+int i, j;
+
+for (i = 0; dest[i] != '\0'; i++)
+;
+
+for (j = 0; j < n && src[j] != '\0'; j++)
+{
+dest[j + i] = src[j];
+}
+
+return (dest);
+
+}
