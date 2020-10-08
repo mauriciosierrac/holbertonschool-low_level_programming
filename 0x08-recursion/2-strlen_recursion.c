3dc97ffb07cbc1fc35eb55adbca48712a655ee67
mauricio.sierrac <2376@holbertonschool.com>
2020-10-08 22:38:15 +0000

    task2

---
+++ 0x08-recursion/2-strlen_recursion.c
@@ -0,0 +1,20 @@
+#include "holberton.h"
+
+/**
+ * _strlen_recursion - print lenght of the string
+ * @s: string
+ **/
+
+int _strlen_recursion(char *s)
+{
+int i = 0;
+if (*s != '\0')
+{
+i =  _strlen_recursion(s + 1);
+return (i + 1);
+}
+else 
+{
+return (0);
+}
+}
