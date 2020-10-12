74b7922dd947182e27a9c44a740de933b0f96a07
mauricio.sierrac <2376@holbertonschool.com>
2020-10-12 00:32:15 +0000

    task0

---
+++ 0x0A-argc_argv/0-whatsmyname.c
@@ -0,0 +1,17 @@
+#include "holberton.h"
+#include <stdio.h>
+
+/**
+ * main - print the name program
+ * @argc: # of arguments
+ * @argv: array
+ * Return: zero
+ **/
+
+int main(int argc, char *argv[])
+{
+
+printf("%s\n", argv[argc - 1]);
+return (0);
+
+}
