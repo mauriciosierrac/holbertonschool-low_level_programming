383547b318b714c33d2407ef6192ef5a53188d2f
mauricio.sierrac <2376@holbertonschool.com>
2020-10-12 01:40:02 +0000

    task3

---
+++ 0x0A-argc_argv/3-mul.c
@@ -0,0 +1,24 @@
+B#include "holberton.h"
+#include <stdlib.h>
+#include <stdio.h>
+
+/**
+ * main - function main
+ * @argc: variable
+ * @argv: variable
+ * Return: zero or one
+ **/
+int main(int argc, char *argv[])
+{
+
+if (argc == 3)
+{
+printf("%d\n", atoi(argv[1]) * atoi(argv[2]));
+return (0);
+}
+else
+{
+printf("Error\n");
+return (1);
+}
+}
