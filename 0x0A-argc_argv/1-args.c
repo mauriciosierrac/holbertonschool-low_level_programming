43b55f087c70bb1cac51c92c669f2e166736bb47
mauricio.sierrac <2376@holbertonschool.com>
2020-10-12 01:12:35 +0000

    task1

---
+++ 0x0A-argc_argv/1-args.c
@@ -0,0 +1,17 @@
+#include "holberton.h"
+#include <stdio.h>
+#include <stdlib.h>
+
+/**
+ * main - function main
+ * @argc: variable
+ * @argv: variable
+ * Return: zero
+ **/
+
+int main(int argc, char *argv[])
+{
+(void) argv;
+printf("%d\n", argc - 1);
+return (0);
+}
