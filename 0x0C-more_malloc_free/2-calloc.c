2b75cc2a9e058ad3c42c2378b41362e25249a582
mauricio.sierrac <2376@holbertonschool.com>
2020-10-15 21:24:58 +0000

    task2

---
+++ 0x0C-more_malloc_free/2-calloc.c
@@ -0,0 +1,32 @@
+#include "holberton.h"
+#include <stdio.h>
+#include <stdlib.h>
+
+/**
+ * _calloc - create matrix
+ * @nmemb: rows
+ * @size: column
+ * Return: pointer to zero
+ **/
+
+void *_calloc(unsigned int nmemb, unsigned int size)
+{
+
+char *s;
+unsigned int i;
+
+if (nmemb == 0 || size == 0)
+{
+return (NULL);
+}
+
+s = malloc(nmemb * size);
+
+for (i = 0; i < nmemb * size; i++)
+{
+s[i] = 0;
+}
+
+return (s);
+
+}
