7b470bc3628c42c11467da802a11a76e0e5a109a
mauricio.sierrac <2376@holbertonschool.com>
2020-10-19 22:48:29 +0000

    task02

--- 0x0E-structures_typedef/2-print_dog.c
+++ 0x0E-structures_typedef/2-print_dog.c
@@ -4,22 +4,32 @@
 /**
  * print_dog - print dates of pets
  * @d: variable pointer
+ *Return: zero
  */
 
 void print_dog(struct dog *d)
 {
+if (d == NULL)
+{
+return;
+}
 if (d->name == NULL)
 {
 printf("Name: (nil)");
 }
+else
+{
+printf("Name: %s\n", d->name);
+}
+printf("Age: %f\n", d->age);
+
 if (d->owner == NULL)
 {
 printf("Owner: (nil)");
 }
-if (d != NULL)
+else
 {
-printf("Name: %s\n", d->name);
-printf("Age: %f\n", d->age);
 printf("Owner: %s\n", d->owner);
 }
+
 }
