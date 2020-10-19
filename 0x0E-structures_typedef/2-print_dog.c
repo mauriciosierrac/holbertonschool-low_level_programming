519d530caf1d67288bf64d8965380b6c0d0df837
mauricio.sierrac <2376@holbertonschool.com>
2020-10-19 22:42:15 +0000

    task02

---
+++ 0x0E-structures_typedef/2-print_dog.c
@@ -0,0 +1,25 @@
+B#include "dog.h"
+#include <stdio.h>
+
+/**
+ * print_dog - print dates of pets
+ * @d: variable pointer
+ */
+
+void print_dog(struct dog *d)
+{
+if (d->name == NULL)
+{
+printf("Name: (nil)");
+}
+if (d->owner == NULL)
+{
+printf("Owner: (nil)");
+}
+if (d != NULL)
+{
+printf("Name: %s\n", d->name);
+printf("Age: %f\n", d->age);
+printf("Owner: %s\n", d->owner);
+}
+}
