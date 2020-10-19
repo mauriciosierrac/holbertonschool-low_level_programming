4c282570993259a49683acf2b91f41bce16a20f0
mauricio.sierrac <2376@holbertonschool.com>
2020-10-19 18:59:56 +0000

    task0

---
+++ 0x0E-structures_typedef/dog.h
@@ -0,0 +1,18 @@
+#ifndef DOG_H
+#define DOG_H
+
+/**
+ * struct dog - structure from dogs dates
+ * @name: parameter of name
+ * @age: parameter of age
+ * @owner: parameter to propietary
+ **/
+
+struct dog
+{
+char *name;
+float age;
+char *owner;
+};
+
+#endif
