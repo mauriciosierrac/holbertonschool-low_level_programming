88307551015dfc1fcd32128d297ea8f411f40dc8
mauricio.sierrac <2376@holbertonschool.com>
2020-10-20 05:00:38 +0000

    task04

--- 0x0E-structures_typedef/4-new_dog.c
+++ 0x0E-structures_typedef/4-new_dog.c
@@ -13,44 +13,35 @@
 
 dog_t *new_dog(char *name, float age, char *owner)
 {
-int  size1, size2, i;
-
 dog_t *dog;
+unsigned int l1 = 0, l2 = 0, i, j;
 
-if (name == NULL || owner == NULL)
-return (NULL);
+dog = malloc(sizeof(dog_t));
+if (name != NULL && owner != NULL && dog != NULL)
+{
+while (name[l1] != 0)
+l1++;
+while (owner[l2] != 0)
+l2++;
 
-dog =  malloc(sizeof(dog_t));
-if (dog == NULL)
-return (NULL);
+dog->name = malloc((l1 + 1) * sizeof(char));
+dog->owner = malloc((l2 + 1) * sizeof(char));
 
-for (size1 = 0; name[size1]; size1++)
-;
-size1++;
-
-dog->name = malloc((size1) *sizeof(char));
-
-if (dog->name == NULL)
-free(dog);
-return (NULL);
-
-for (i = 0; i < size1; i++)
-dog->name[i] = name[i];
-dog->age = age;
-
-for (size2 = 0; owner[size2]; size2++)
-;
-size2++;
-dog->owner = malloc((size2) *sizeof(char));
-
-if (dog->owner == NULL)
+if (dog->name == NULL || dog->owner == NULL)
 {
 free(dog->name);
+free(dog->owner);
 free(dog);
 return (NULL);
 }
-
-for (i = 0; i < size2; i++)
-dog->owner[i] = owner[i];
+for (i = 0; i < l1; i++)
+dog->name[i] = name[i];
+dog->name[i] = 0;
+dog->age = age;
+for (j = 0; j < l2; j++)
+dog->owner[j] = owner[j];
+dog->owner[j] = 0;
 return (dog);
+}
+return (NULL);
 }
