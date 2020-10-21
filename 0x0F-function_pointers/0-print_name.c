a2a3e59bbcf4b0748486fc7b33dc52304d51b3e4
mauricio.sierrac <2376@holbertonschool.com>
2020-10-21 16:08:02 +0000

    task0

---
+++ 0x0F-function_pointers/0-print_name.c
@@ -0,0 +1,12 @@
+#include "function_pointers.h"
+#include <stdio.h>
+
+/**
+ * print_name - function that print a name
+ * @name: variable parameter
+ * @f: pointer to function
+ */
+void print_name(char *name, void (*f)(char *))
+{
+(*f)(name);
+}
