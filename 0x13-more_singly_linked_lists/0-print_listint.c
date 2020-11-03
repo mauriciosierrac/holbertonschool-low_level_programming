5364f7dd82c39b03dfd0c4b64d0a1c8fb9540521
mauricio.sierrac <2376@holbertonschool.com>
2020-11-03 16:52:20 +0000

    task01 file

---
+++ 0x13-more_singly_linked_lists/0-print_listint.c
@@ -0,0 +1,27 @@
+#include "lists.h"
+
+/**
+ * print_listint - print the elements of list and numbers of nodes
+ * @h: pointer to list
+ * Return: numbers of nodes
+ */
+
+size_t print_listint(const listint_t *h)
+{
+	size_t i = 0;
+
+	while (h != NULL)
+	{
+		if (h->n == '\0')
+		{
+			return (-1);
+		}
+		else
+		{
+			printf("%d\n", h->n);
+		}
+		h = h->next;
+		i++;
+	}
+	return (i);
+}
