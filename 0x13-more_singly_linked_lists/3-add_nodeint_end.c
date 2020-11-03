056cbd3d69aad7f938e8c937a5f23a2cb3906458
mauricio.sierrac <2376@holbertonschool.com>
2020-11-03 23:35:33 +0000

    fixed task03

--- 0x13-more_singly_linked_lists/3-add_nodeint_end.c
+++ 0x13-more_singly_linked_lists/3-add_nodeint_end.c
@@ -11,8 +11,9 @@ listint_t *add_nodeint_end(listint_t **head, const int n)
 	listint_t *node = NULL, *aux = NULL;
 
 	node = malloc(sizeof(listint_t));
+
 	if (node == NULL)
-		return NULL;
+		return (NULL);
 
 	node->n = n;
 	node->next = NULL;
