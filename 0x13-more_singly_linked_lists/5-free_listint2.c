851f6b74f76ed5f3ef23be627566826bc9ad5ad4
mauricio.sierrac <2376@holbertonschool.com>
2020-11-04 14:54:33 +0000

    fixed task05

--- 0x13-more_singly_linked_lists/5-free_listint2.c
+++ 0x13-more_singly_linked_lists/5-free_listint2.c
@@ -9,6 +9,10 @@ void free_listint2(listint_t **head)
 {
 	listint_t *node;
 
+	if (head == NULL)
+	{
+		return;
+	}
 	while (*head)
 	{
 		node = *head;
@@ -16,9 +20,5 @@ void free_listint2(listint_t **head)
 		free(node);
 	}
 
-	if (head == NULL)
-	{
-		return;
-	}
 	*head = NULL;
 }
