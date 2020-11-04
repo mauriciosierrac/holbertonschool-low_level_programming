4221fa9639652213a52e6014d3b2c34a0e57a44a
mauricio.sierrac <2376@holbertonschool.com>
2020-11-04 15:35:46 +0000

    file task07

---
+++ 0x13-more_singly_linked_lists/7-get_nodeint.c
@@ -0,0 +1,27 @@
+#include "lists.h"
+
+/**
+ * get_nodeint_at_index - function that return N element
+ * @head: pointer to list
+ * @index: element parameter to print
+ * Return: element N of list
+ */
+listint_t *get_nodeint_at_index(listint_t *head, unsigned int index)
+{
+	unsigned int i = 0;
+	listint_t *node;
+
+	if (head == NULL)
+	{
+		return (NULL);
+	}
+
+	while (i <= index)
+	{
+		node = head;
+		head = head->next;
+		i++;
+	}
+	return (node);
+}
+
