3c1dd9095221058cde7799a45be8492678184e2f
mauricio.sierrac <2376@holbertonschool.com>
2020-11-05 20:26:48 +0000

    task0  file

---
+++ 0x14-bit_manipulation/0-binary_to_uint.c
@@ -0,0 +1,28 @@
+#include "holberton.h"
+
+/**
+ * binary_to_uint - convert string binary in dec
+ * @b: pointer to string
+ * Return: value in base10
+ */
+unsigned int binary_to_uint(const char *b)
+{
+	int i, j;
+	int pot = 1, value = 0;
+
+	for (i = 0; b[i] != '\0'; i++)
+		;
+	for (j = (i - 1); j >= 0; j--)
+	{
+		if (b[j] != '0' && b[j] != '1')
+		{
+			return (0);
+		}
+		if (b[j] == '1')
+		{
+			value += 1 * pot;
+		}
+		pot *= 2;
+	}
+	return (value);
+}
