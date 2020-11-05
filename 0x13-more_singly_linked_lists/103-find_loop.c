b04dbe7203c75f3b612645530c6a97133a332eb5
mauricio.sierrac <2376@holbertonschool.com>
2020-11-05 04:56:23 +0000

    103avanced

---
+++ 0x13-more_singly_linked_lists/103-find_loop.c
@@ -0,0 +1,11 @@
+#include "lists.h"
+
+/**
+ * free_listint_loop - print the length of list
+ * @head: pointer to list
+ * Return: length of a list
+ */
+
+listint_t *find_listint_loop(listint_t *head)
+{
+}
