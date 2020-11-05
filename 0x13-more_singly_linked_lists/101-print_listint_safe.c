ba749d1100336adb44bd36f4126bc3b447b564fc
mauricio.sierrac <2376@holbertonschool.com>
2020-11-05 04:55:36 +0000

    101avanced

---
+++ 0x13-more_singly_linked_lists/101-print_listint_safe.c
@@ -0,0 +1,11 @@
+#include "lists.h"
+
+/**
+ * print_listint_safe - print the length of list
+ * @head: pointer to list
+ * Return: length of a list
+ */
+
+size_t print_listint_safe(const listint_t *head)
+{
+}
