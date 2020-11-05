81ec28c249975562c72b5c16296b27b03f78976e
mauricio.sierrac <2376@holbertonschool.com>
2020-11-05 03:29:00 +0000

    file task09

---
+++ 0x13-more_singly_linked_lists/9-insert_nodeint.c
@@ -0,0 +1,42 @@
+#include "lists.h"
+
+/**
+ * insert_nodeint_at_index - add other node in N position
+ * @head: pointer to pointer list
+ * @idx: value to inster in list
+ * @n: position to added
+ * Return: value of list
+ */
+listint_t *insert_nodeint_at_index(listint_t **head, unsigned int idx, int n)
+{
+
+	listint_t *node1, *node2;
+	unsigned int i = 0;
+
+	node1 = malloc(sizeof(listint_t));
+	if (!node1 || (!(*head) && idx > 0))
+		return (NULL);
+
+	node1->n = n;
+	if (idx == 0)
+	{
+		node1->next = *head;
+		*head = node1;
+		return (node1);
+	}
+
+	node2 = *head;
+
+	for (i = 0; node1 && i < idx; i++)
+	{
+		if (i == idx - 1)
+		{
+			node1->next = node2->next;
+			node2->next = node1;
+			return (node1);
+		}
+		node2 = node2->next;
+	}
+	free(node1);
+	return (NULL);
+}
