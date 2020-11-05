2c5a035fc6c0a61dd57aed5e853a81c7f8706f4a
mauricio.sierrac <2376@holbertonschool.com>
2020-11-05 20:28:47 +0000

    task0  file

--- 0x14-bit_manipulation/0-binary_to_uint.c
+++ 0x14-bit_manipulation/0-binary_to_uint.c
@@ -10,6 +10,10 @@ unsigned int binary_to_uint(const char *b)
 	int i, j;
 	int pot = 1, value = 0;
 
+	if (b == NULL)
+	{
+		return (0);
+	}
 	for (i = 0; b[i] != '\0'; i++)
 		;
 	for (j = (i - 1); j >= 0; j--)
