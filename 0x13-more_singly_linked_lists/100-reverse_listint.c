34bd7a9c5724d5c1b06913168f3b836ac5e09e4c
mauricio.sierrac <2376@holbertonschool.com>
2020-11-05 04:55:05 +0000

    100avanced

---
+++ 0x13-more_singly_linked_lists/100-reverse_listint.c
@@ -0,0 +1,11 @@
+#include "lists.h"
+
+/**
+ * reverse_listint - print the length of list
+ * @head: pointer to list
+ * Return: length of a list
+ */
+
+listint_t *reverse_listint(listint_t **head)
+{
+}
