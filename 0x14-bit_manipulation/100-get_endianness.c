c6a7ca05ebd0bf0834dcb7223aba792d504ec6d9
mauricio.sierrac <2376@holbertonschool.com>
2020-11-06 03:08:52 +0000

    task 100 file

---
+++ 0x14-bit_manipulation/100-get_endianness.c
@@ -0,0 +1,15 @@
+#include "holberton.h"
+
+/**
+ * get_endianness - function that print little endian or big endian
+ * Return: 1 or 0
+ */
+
+int get_endianness(void)
+{
+
+	unsigned int x = 1;
+	char *c = (char *)&x;
+
+	return (int)*c;
+}
