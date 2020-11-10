71dba852bb99b9083cacb1a072c29885a8861bfe
mauricio.sierrac <2376@holbertonschool.com>
2020-11-10 05:14:57 +0000

    fixed file task03

--- 0x15-file_io/3-cp.c
+++ 0x15-file_io/3-cp.c
@@ -16,21 +16,24 @@ int main(int argc, char **arg)
 		dprintf(STDERR_FILENO, "Usage: cp file_from file_to\n"), exit(97);
 
 	f1 = open(arg[1], O_RDONLY);
+	if (f1 == -1)
+		dprintf(STDERR_FILENO, "Error: Can't read from file %s\n", arg[1]), exit(98);
 
 	f2 = open(arg[2], O_CREAT | O_WRONLY | O_TRUNC, 0664);
+	if (f2 == -1)
+		dprintf(STDERR_FILENO, "Error: Can't write to %s\n", arg[2]), exit(99);
 
-	while (rf1 == 1024)
-	{
+	do {
 		rf1 = read(f1, buf, 1024);
-		wf2 = write(f1, buf, rf1);
-	}
+		wf2 = write(f2, buf, rf1);
+	} while (rf1 == 1024);
 
-	if (rf1 == -1 || f1 == -1)
+	if (rf1 == -1)
 	{
 		dprintf(STDERR_FILENO, "Error: Can't read from file %s\n", arg[1]), exit(98);
 	}
 
-	if (wf2 == -1 || f2 == -1)
+	if (wf2 == -1)
 	{
 		dprintf(STDERR_FILENO, "Error: Can't write to %s\n", arg[2]), exit(99);
 	}
