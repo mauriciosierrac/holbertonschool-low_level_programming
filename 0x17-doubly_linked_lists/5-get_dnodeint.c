f3f508c090a39fc57942a19bcdc3bf0aa30797b9
mauricio.sierrac <2376@holbertonschool.com>
2020-12-14 14:09:04 +0000

    task 5 :v:

--- 0x17-doubly_linked_lists/5-get_dnodeint.c
+++ 0x17-doubly_linked_lists/5-get_dnodeint.c
@@ -1,11 +1,30 @@
 #include "lists.h"
 
 /**
- * get_dnodeint_at_index -
- *
- * Return: 
+ * get_dnodeint_at_index - return N element
+ * @head: pointer to list
+ * @index: element parameter to print
+ * Return: elemen N of list
  */
 dlistint_t *get_dnodeint_at_index(dlistint_t *head, unsigned int index)
 {
+	unsigned int i = 0;
+	dlistint_t *node;
 
+	if (head == NULL)
+	{
+		return (NULL);
+	}
+
+	while (i <= index)
+	{
+		if (head == NULL)
+		{
+			return (NULL);
+		}
+		node = head;
+		head = head->next;
+		i++;
+	}
+	return (node);
 }
