23eb119178efdd0afe8616e28441262c5836d292
mauricio.sierrac <2376@holbertonschool.com>
2020-12-14 14:15:20 +0000

    task6 :v:

--- 0x17-doubly_linked_lists/6-sum_dlistint.c
+++ 0x17-doubly_linked_lists/6-sum_dlistint.c
@@ -1,11 +1,24 @@
 #include "lists.h"
 
 /**
- * sum_dlistint -
- *
- * Return: 
+ * sum_dlistint - function that sum the parameters of list
+ * @head: pointer to list
+ * Return: value of sum
  */
 int sum_dlistint(dlistint_t *head)
 {
+	dlistint_t *node;
+	int add = 0;
 
+	while (head)
+	{
+		if (head == NULL)
+		{
+			return (NULL);
+		}
+		node = head;
+		head = head->next;
+		add += node->n;
+	}
+	return (add);
 }
