ee3c2e5bdeb1488c4e0d033fbaa8ece9c35742c1
mauricio.sierrac <2376@holbertonschool.com>
2021-04-19 22:51:41 +0000

    create task00

---
+++ 0x1E-search_algorithms/0-linear.c
@@ -0,0 +1,31 @@
+#include "search_algos.h"
+#include <stdio.h>
+#include <stdlib.h>
+
+/**
+ * linear_search - make a lineal shearch of a value in array
+ * @array: matrix of values
+ * @size: lenght of value
+ * @value: value to search in the array
+ * Return: -1 if not found or position of array with found value
+ */
+
+int linear_search(int *array, size_t size, int value)
+{
+	size_t i;
+
+	if (array == NULL)
+	{
+		return (-1);
+	}
+
+	for (i = 0; i < size; i++)
+	{
+		printf("Value checked array[%ld] = [%d]\n", i, array[i]);
+		if (array[i] == value)
+		{
+			return (i);
+		}
+	}
+	return (-1);
+}
